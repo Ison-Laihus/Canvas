b1481e43434cb6965c06a926a44c105d4dbd361c
Ison-Laihus <IsoIceIno7@gmail.com>
2016-09-21 20:38:13 +0800

    Update image.js

--- image.js
+++ image.js
@@ -1,26 +1,37 @@
+// 定义canvas的宽高
 var canvasWidth = 800;
 var canvasHeight = 600;
+
+// 设置变量描述show()函数是否执行
 var isRunningShow = 0;
+
+// 设置两个动画的全局变量
 var animation = '';
 var animation2 = '';
 
+// 设置剪辑的坐标和半径属性
+var clippingRegion = {x:400, y:200, r:0};
+var radius = 50;
+
+// 获取canvas与绘图环境对象context
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 
+// 设置canvas的宽高
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 
-var radius = 50;
+// 实例化图片对象，设置图片属性
 var image = new Image();
-var clippingRegion = {x:400, y:200, r:0};
-
-
 image.src = "image.jpg";
+
+// 加载图片，加载完成后立即初始化画布
 image.onload = function(e) {
 	initCanvas();
 }
 
+// 初始化画布
 function initCanvas() {
 	clippingRegion.r = 0;
 	if ( isRunningShow ) {
@@ -41,6 +52,7 @@ function initCanvas() {
 	draw(image, clippingRegion);
 }
 
+// 在画布上进行绘制---向画布上绘制图片以及进行剪辑
 function draw(image, clippingRegion) {
 	context.clearRect(0, 0, canvasWidth, canvasHeight);
 	context.save();
@@ -49,12 +61,14 @@ function draw(image, clippingRegion) {
 	context.restore();
 }
 
+// 设置剪辑区域的属性---坐标和半径
 function setClippingRegion() {
 	context.beginPath();
 	context.arc(clippingRegion.x, clippingRegion.y, clippingRegion.r, 0, Math.PI*2, false);
 	context.clip();
 }
 
+// 将剪辑的清晰区域展开
 function show() {
 	isRunningShow = 1;
 	animation = setInterval(function(){
@@ -68,6 +82,7 @@ function show() {
 	}, 50);
 }
 
+// 重置剪辑区域
 function reset() {
 	initCanvas();
 }
